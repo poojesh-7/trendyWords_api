0a7cfc4921d732d4cbbe89a4676f6b0112fb507a
1pooj234 <kspoojesh5@gmail.com>
2025-12-21 23:44:23 +0530

    redis, rmq and worker setup

---
+++ config/redis.js
@@ -0,0 +1,30 @@
+const { createClient } = require("redis");
+
+const redisClient = createClient({
+  url: process.env.REDIS_URL,
+});
+
+redisClient.on("connect", () => {
+  console.log("Redis connected");
+});
+
+redisClient.on("error", (err) => {
+  console.error("Redis error:", err);
+});
+
+let isConnected = false;
+
+const connectRedis = async () => {
+  if (isConnected) return;
+
+  try {
+    await redisClient.connect();
+    isConnected = true;
+  } catch (err) {
+    console.error("Failed to connect Redis:", err.message);
+  }
+};
+
+connectRedis();
+
+module.exports = redisClient;
